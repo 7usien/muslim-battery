d29aebd2cdf2db886b2e048016a2bcd57d31636b
7usien <husienadel2022@gmail.com>
2022-01-30 21:08:48 +0300

    update-js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,32 @@
-
 const content = document.querySelector('.content');
 const doaaCount = document.querySelector('.card__count');
 const cards = document.querySelectorAll('.card');
 
 
-
 class Doaa {
-
-  fetchData = async () => {
-
-    const respons = await fetch('../json/azkar.json');
-    const data = await respons.json();
-    return data
-
+  constructor(url) {
+    this.url = url;
   }
 
-
+  fetchData = async () => {
+    const response = await fetch(this.url);
+    const data = await response.json();
+    return data;
+  };
 
 
 }
 
-const doaa = new Doaa();
-doaa.fetchData()
-  .then(doaas => {
+const doaa = new Doaa('../json/azkar.json');
+doaa.fetchData().then((doaas) => {
+  const filteredDoaa = Array.from(doaas).filter((doaa) => {
+    if (doaa['category'] === 'أذكار الصباح') {
+      return doaa;
+    }
+  });
 
-
-    const filteredDoaa = Array.from(doaas).filter((doaa) => {
-      if (doaa['category'] === 'أذكار الصباح') {
-        return doaa
-      }
-
-    });
-
-    const totalCount = filteredDoaa.length;
-    filteredDoaa.forEach((doaa) => {
-
-
-      const card = `
+  filteredDoaa.forEach((doaa) => {
+    const card = `
       <div class="card">
           <h2 class="card__title">
 
@@ -48,10 +38,10 @@ doaa.fetchData()
 
           <div class="card__nav">
 
-            <span class="card__nav--right">الدعاء التالي</span>
+            <span class="card__nav--right">→ الدعاء التالي</span>
             <span class="card__count">عدد مرات التكرار :    ${doaa.count}</span>
 
-            <span class="card__nav--left">الدعاء السابق</span>
+            <span class="card__nav--left">الدعاء السابق ←</span>
 
           </div>
         </div>
@@ -59,17 +49,61 @@ doaa.fetchData()
       
       `;
 
-      content.innerHTML += card;
+    content.innerHTML += card;
+
+  });
+
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach((card) => {
+    card.parentElement.querySelector('.card').classList.remove('hidden');
+    card.classList.add('hidden');
+  });
+  let countUp = 1;
+  let countDown = cards.length;
+
+
+  const cardNavRight = document.querySelectorAll('.card__nav--right');
+  const cardNavleft = document.querySelectorAll('.card__nav--left');
 
 
 
 
-    })
-
-  })
-
+  cardNavRight.forEach((item) => {
+    item.addEventListener('click', (e) => {
 
 
+      if (countUp < countDown) {
+
+        e.target.parentElement.parentElement.classList.add('hidden');
+        e.target.parentElement.parentElement.nextElementSibling.classList.remove(
+          'hidden'
+        );
+        countUp++;
+      }
+
+      if (countUp === countDown) {
+        alert('تم أنهاء قراءة الأذكار بنجاح ..اللهم تقبل منكم')
+      }
+    });
+  });
 
 
+
+  cardNavleft.forEach((item) => {
+    item.addEventListener('click', (e) => {
+
+      if (countUp > 1) {
+
+        e.target.parentElement.parentElement.classList.add('hidden');
+        e.target.parentElement.parentElement.previousElementSibling.classList.remove(
+          'hidden'
+        );
+        countUp--;
+      }
+    });
+  });
+
+
+});
 
